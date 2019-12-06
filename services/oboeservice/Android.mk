75106ee0ba437b65d81ffa77cffc69f1a16d9626
Sasha Smundak <asmundak@google.com>
2019-12-06 12:39:32 -0800

    Remove setting LOCAL_MULTILIB from AUDIOSERVER_MULTILIB
    Bug: 121208203
    Test: treehugger
    Change-Id: Ia8a640c4e59cd33281e88ab379a119e1d120f096
    Merged-In: I263b6b302f7220e4b712924d2df2fd2548af03b1
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/oboeservice/Android.mk
+++ services/oboeservice/Android.mk
@@ -40,8 +40,6 @@ LOCAL_SRC_FILES += \
     TimestampScheduler.cpp \
     AAudioThread.cpp
 
-LOCAL_MULTILIB := $(AUDIOSERVER_MULTILIB)
-
 # LOCAL_CFLAGS += -fvisibility=hidden
 LOCAL_CFLAGS += -Wno-unused-parameter
 LOCAL_CFLAGS += -Wall -Werror
